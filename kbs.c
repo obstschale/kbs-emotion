8a32cec24ff1cc43e33b9e745d407758c0e1c6fc
Hans-Helge Buerger <mail@hig-podcast.de>
2014-01-10 14:32:17 +0100

    save data in structs
    get string from file and use a tokenizer to separate data. Convert
    strings to integer and save it for eacht set in new memory.

--- kbs.c
+++ kbs.c
@@ -1,21 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+struct set
+{
+	int frame;
+	int eye;
+	int forehead;
+	int mouth;
+	struct set * prev;
+	struct set * next;
+};
+
+void printSet( struct set * obs);
 
 int main(int argc, char const *argv[])
 {
 	FILE *fp;
 	char buffer[80];
-	int index;
+	struct set * observation;
+	struct set * temp;
+	char * pch;
 
 	// open read only
 	fp = fopen("Beispieldatei/E_013.csv","r");
 	if (fp == NULL) {
 		perror( "Error opening file" );
 	} else {
-		// loop thru file til EOF 
+		observation = (struct set*) malloc( sizeof(struct set) );
+		if (observation == NULL) exit (1);
+		observation->prev = NULL;
+		observation->next = NULL;
+
+		// Read first line to skip titles
+		// loop start with first line of numbers
+		fscanf(fp, "%s", buffer);
+
+		// loop thru file til EOF
 		while ( fscanf(fp, "%s", buffer) != EOF ) {
-			printf("%s\n", buffer);
+			// save data in sets
+			pch = strtok( buffer, ";" );
+			observation->frame = atoi( pch );
+			// printf( "%i\n", observation->frame );
+			pch = strtok( NULL, ";" );
+			observation->eye = atoi( pch );
+			pch = strtok( NULL, ";" );
+			observation->forehead = atoi( pch );
+			pch = strtok( NULL, ";" );
+			observation->mouth = atoi( pch );
+			// printSet(observation);
+
+			// allocate new memory for next entry
+			temp = (struct set*) malloc( sizeof(struct set) );
+			observation->next = temp;
+			temp->prev = observation;
+			temp->next = NULL;
+			observation = temp;
 		}
 	}
 
+	do {
+		observation = observation->prev;
+	} while ( observation->prev != NULL );
+	while ( observation->next != NULL ) {
+		printSet( observation );
+		observation = observation->next;
+	}
+
 	return 0;
+}
+
+void printSet( struct set * obs) {
+	printf("---Set #%i---\n", obs->frame);
+	printf("Eye: %i\n", obs->eye);
+	printf("Forehead: %i\n", obs->forehead);
+	printf("Mouth: %i\n", obs->mouth);
+	printf("\n");
 }
