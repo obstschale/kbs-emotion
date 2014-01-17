1783bfb3671b667e65e2e5faf3f72c419a4594ca
Hans-Helge Buerger <mail@hig-podcast.de>
2014-01-17 09:20:58 +0100

    Add header file
    put struct, print_set() and show_all_sets() in header files.
    Also introduce start ptr which always points to first entry.

--- kbs.c
+++ kbs.c
@@ -1,37 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "kbs.h"
 
-
-struct set
-{
-	int frame;
-	int eye;
-	int forehead;
-	int mouth;
-	struct set * prev;
-	struct set * next;
-};
-
-void printSet( struct set * obs);
 
 int main(int argc, char const *argv[])
 {
 	FILE *fp;
 	char buffer[80];
+	struct set * start;
 	struct set * observation;
 	struct set * temp;
 	char * pch;
 
 	// open read only
-	fp = fopen("Beispieldatei/E_013.csv","r");
+	fp = fopen("Files/E_013_Beispieldatei.csv","r");
 	if (fp == NULL) {
 		perror( "Error opening file" );
 	} else {
-		observation = (struct set*) malloc( sizeof(struct set) );
-		if (observation == NULL) exit (1);
+		start = (struct set*) malloc( sizeof(struct set) );
+		if (start == NULL) exit (1);
+		observation = start;
 		observation->prev = NULL;
 		observation->next = NULL;
+
 
 		// Read first line to skip titles
 		// loop start with first line of numbers
@@ -49,7 +38,7 @@ int main(int argc, char const *argv[])
 			observation->forehead = atoi( pch );
 			pch = strtok( NULL, ";" );
 			observation->mouth = atoi( pch );
-			// printSet(observation);
+			// print_set(observation);
 
 			// allocate new memory for next entry
 			temp = (struct set*) malloc( sizeof(struct set) );
@@ -60,21 +49,6 @@ int main(int argc, char const *argv[])
 		}
 	}
 
-	do {
-		observation = observation->prev;
-	} while ( observation->prev != NULL );
-	while ( observation->next != NULL ) {
-		printSet( observation );
-		observation = observation->next;
-	}
-
+	// show_all_sets( start );
 	return 0;
-}
-
-void printSet( struct set * obs) {
-	printf("---Set #%i---\n", obs->frame);
-	printf("Eye: %i\n", obs->eye);
-	printf("Forehead: %i\n", obs->forehead);
-	printf("Mouth: %i\n", obs->mouth);
-	printf("\n");
 }
---
+++ kbs.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct set
+{
+	int frame;
+	int eye;
+	int forehead;
+	int mouth;
+	struct set * prev;
+	struct set * next;
+};
+
+void print_set( struct set * obs) {
+	printf("---Set #%i---\n", obs->frame);
+	printf("Eye: %i\n", obs->eye);
+	printf("Forehead: %i\n", obs->forehead);
+	printf("Mouth: %i\n", obs->mouth);
+	printf("\n");
+}
+
+void show_all_sets( struct set * ptr ) {
+	while ( ptr->next != NULL ) {
+		print_set( ptr );
+		ptr = ptr->next;
+	}
+}
